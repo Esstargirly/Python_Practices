d2639097a62feb325f5aa1600ef77f5a93c006ea
Esstargirly <estherappolos51@gmail.com>
2026-01-16 23:27:52 +0100

    practice on input function

---
+++ input.py
@@ -0,0 +1,43 @@
+# print("Tell me anything...")
+# anything = input()
+# print("Hmm...", anything, "... Really?")
+
+# anything = input("Tell me anything...")
+# print("Hmm...", anything, "...Really?")
+
+# anything = float(input("Enter a number: "))
+# something = anything ** 2.0
+# print(anything, "to the power of 2 is", something)
+
+# leg_a = int(input("Input first leg length: "))
+# leg_b = float(input("Input second leg length: "))
+# hypo = (leg_a**2 + leg_b**2) ** .5
+# print("Hypotenuse length is", hypo)
+
+# fnam = input("May I have your first name, please? ")
+# lnam = input("May I have your last name, please? ")
+# print("Thank you.")
+# print("\nYour name is " + fnam + " " + lnam + ".")
+
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
+
+# leg_a = float(input("Input first leg length: "))
+# leg_b = float(input("Input second leg length: "))
+# print("Hypotenuse length is " + str((leg_a**2 + leg_b**2) ** .5))
+
+# input a float value for variable a here
+a = float(input("Enter first value: "))
+# input a float value for variable b here
+b = float(input("Enter second value: "))
+
+# output the result of addition here
+print ("Addition", a + b)
+# output the result of subtraction here
+print("Subtraction", a-b)
+# output the result of multiplication here
+print("Multiplication", a*b)
+# output the result of division here
+print("Division", a/b)
+print("\nThat's all, folks!")

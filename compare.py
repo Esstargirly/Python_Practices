29d8255bbcaaf8a200765fa3f6c296f2cd176538
Esstargirly <estherappolos51@gmail.com>
2026-01-18 22:47:19 +0100

    i learnt about comparison operators

---
+++ compare.py
@@ -0,0 +1,15 @@
+var = 0  # Assigning 0 to var
+print(var == 0)
+
+var = 1  # Assigning 1 to var
+print(var == 0)
+
+var = 0  # Assigning 0 to var
+print(var != 0)
+
+var = 1  # Assigning 1 to var
+print(var != 0)
+
+
+n = int(input("Enter n "))
+print( n >= 100)

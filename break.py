8fcb04ec2ce8e769a4d4277cf7c3877a020b63d8
Esstargirly <estherappolos51@gmail.com>
2026-01-26 15:38:28 +0100

    i learned about conditional statements

---
+++ break.py
@@ -0,0 +1,26 @@
+# # break - example
+
+# print("The break instruction:")
+# for i in range(1, 6):
+#     if i == 3:
+#         break
+#     print("Inside the loop.", i)
+# print("Outside the loop.")
+
+
+# # continue - example
+
+# print("\nThe continue instruction:")
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print("Inside the loop.", i)
+# print("Outside the loop.")
+
+
+secret_word = "chupacabra"
+while True:
+    word = input("Enter the secret word ")
+    if word == secret_word:
+        print ("You've successfully left the loop.")
+        break
---
+++ continue2.py
@@ -0,0 +1,30 @@
+# # Prompt the user to enter a word
+# user_word = input("Enter a word ")
+# # and assign it to the user_word variable.
+# user_word = user_word.upper()
+
+# vowel = "AEIOU"
+# for letter in user_word:
+#     # Complete the body of the for loop.
+#     if letter in vowel:
+#         continue
+#     print(letter)
+
+
+     
+word_without_vowels = ""
+
+# Prompt the user to enter a word
+user_word = input("Enter a word ")
+# and assign it to the user_word variable.
+user_word = user_word.upper()
+
+vowels = "AEIOU"
+for letter in user_word:
+    # Complete the body of the loop.
+    if letter in vowels:
+        continue
+    word_without_vowels += letter
+# Print the word assigned to word_without_vowels.
+print(word_without_vowels)
+

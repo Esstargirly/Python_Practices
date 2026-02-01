4511a394bab7361a64026356481f081fcc880a48
Esstargirly <estherappolos51@gmail.com>
2026-02-01 22:12:04 +0100

    i learned about list in list

---
+++ listinlist.py
@@ -0,0 +1,39 @@
+# A four-column/four-row table ‒ a two dimensional array (4x4)
+
+table = [[2, 5, 4, 1],
+         [6, 8, 5, 9],
+         [8, 1, 11, 0],
+         [8, 7, 2, 4]]
+
+print(table)
+print(table[0][0]) 
+print(table[0][3])  
+
+###
+
+# Cube - a three-dimensional array (3x3x3)
+
+cube = [[[1, 'x', 'x'],
+         [6, 'x', 'x'],
+         [0, 'x', 'x']],
+
+        [[3, 'x', 'x'],
+         [2, 'm', 'f'],
+         [0, 'x', 'x']],
+
+        [[7, 'x', 'x'],
+         [8, 'x', 'x'],
+         [4, 'x', 'x']]]
+
+print(cube)
+print(cube[0][0][0])  
+print(cube[2][2][0])
+
+
+
+
+
+
+
+
+

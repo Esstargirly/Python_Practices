46efbd7a8e6e83865bffe97b1f0e152693189a5d
Esstargirly <estherappolos51@gmail.com>
2026-02-02 22:35:42 +0100

    i learnt about functions

---
+++ func.py
@@ -0,0 +1,17 @@
+def message():
+    print("Enter a value: ")
+
+print("We start here.")
+message()
+print("We end here.")
+
+def message():
+    print("Enter a value: ")
+
+message()
+a = int(input())
+message()
+b = int(input())
+message()
+c = int(input())
+
